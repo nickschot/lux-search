d8d617e1e94b54845782b8b45720da9c98977d70
Nick Schot <nickschot@gmail.com>
2017-05-17 23:27:28 +0200

    perf: add more index tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -10,19 +10,19 @@ describe('search', () => {
     }))
   };
 
+  const model = {
+    tableName: 'test_table',
+    columnFor: jest.fn().mockImplementation((column) => ({
+      'type': 'varchar',
+      'columnName': underscore(column)
+    })),
+    store: {
+      config: {}
+    }
+  };
+
   test('does not work when an unsupported driver is used', () => {
-    const model = {
-      tableName: 'test_table',
-      columnFor: jest.fn().mockImplementation((column) => ({
-        'type': 'varchar',
-        'columnName': underscore(column)
-      })),
-      store: {
-        config: {
-          driver: 'fake_driver'
-        }
-      }
-    };
+    model.store.config.driver = 'fake_driver';
 
     const request = {
       controller: {
@@ -40,5 +40,39 @@ describe('search', () => {
     expect(() => {
       search(query, request, columns)
     }).toThrowError(`[lux-search] Database driver not supported. Please use one of the following: pg, mysql, mysql2, mariasql, sqlite3`);
+  });
+
+  test('gracefully skips if no search param is present', () => {
+    model.store.config.driver = 'pg';
+
+    const request = {
+      controller: {
+        model: model
+      },
+      params: {}
+    };
+
+    const columns = ['name'];
+
+    expect(search(query, request, columns)).toEqual(query);
+  });
+
+  test('gracefully skips if no columns are present', () => {
+    model.store.config.driver = 'pg';
+
+    const request = {
+      controller: {
+        model: model
+      },
+      params: {
+        'search': {
+          'name': 'John Doe'
+        }
+      }
+    };
+
+    const columns = [];
+
+    expect(search(query, request, columns)).toEqual(query);
   });
 });
